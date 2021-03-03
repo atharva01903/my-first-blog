b5fdc685a7e0d1858fcb5b2694cfcc426068b981
atharva01903 <atharvayogesh.d19@iiits.in>
2021-03-04 00:13:34 +0530

    Modified templates to display posts from database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u85ln$zrfev%c3&*!s_3(-+$hd0tucn5+!^p#=v&tlfr8gm#iv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
